343de0277aed05088cd7d1cacfb9a58655fe1685
manavendrasen <manavendra.199302058@muj.manipal.edu>
2020-11-12 05:04:10 +0530

    added nodemailer and mail on form submit

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const routes = require("./routes/routes");
 const app = express();
 const mongoose = require("mongoose");
 const md5 = require("md5");
-
+const nodemailer = require("nodemailer");
+const output = require("./public/email/email");
 app.use(bodyParser.urlencoded({ extended: false }));
 let port = process.env.PORT;
 if (port == null || port == "") {
@@ -46,7 +47,6 @@ var userSchema = new mongoose.Schema({
 const Message = mongoose.model("Message", messageSchema);
 app.post("/", (req, res) => {
   const name = req.body.iname;
-
   const email = req.body.iemail;
   const message = req.body.imessage;
 
@@ -61,6 +61,60 @@ app.post("/", (req, res) => {
   mess.save();
   const page_name = "home";
   const success = "true";
+
+  async function sendEmail() {
+    // const output = `
+    // <h2>Thank you ${name} for getting in touch!</h2>
+    // <hr/>
+    // <p>We appreciate you contacting us. We will get back in touch with you soon!</p>
+    // <p>Cheers!</p>
+    // <p>Spacsec'21 Team</p>
+    // `;
+    const outputForBackend = `
+    <h2>Contact form filled</h2>
+    <h4>We received message on spacsec.com: </h4>
+    <hr/>
+    <p>Name: ${name}</p>
+    <p>Email: <a href="mailto:${email}">${email}</a></p>
+    <p>Message:</p>
+    <p>${message}</p>
+    <hr/>
+    <p>
+    Visit <a href="https://spacsec.com/login/">here</a> to view all the form responses.
+    </p>
+    `;
+    let transporter = nodemailer.createTransport({
+      name: "spacsec.com",
+      host: "mail.spacsec.com",
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: "noreply@spacsec.com", // generated ethereal user
+        pass: "*C!RNJSak2lF", // generated ethereal password
+      },
+      tls: {
+        rejectUnauthorized: false,
+      },
+    });
+    console.log("made the mail");
+    let info = await transporter.sendMail({
+      from: '"SpacSec Team" <noreply@spacsec.com>', // sender address
+      to: email,
+      subject: "Thanks for contacting us!", // Subject line
+      html: output(name), // html body
+    });
+    let infoBackendMail = await transporter.sendMail({
+      from: '"Website Response" <noreply@spacsec.com>', // sender address
+      to: "manavendra4288@gmail.com",
+      subject: "Contact Form Submitted", // Subject line
+      html: outputForBackend, // html body
+    });
+
+    console.log("Message sent: %s", info.messageId);
+    console.log("Message sent: %s", infoBackendMail.messageId);
+    // console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  }
+  sendEmail().catch(console.error);
   res.render("index", { page_name: page_name, success: success });
 });
 
@@ -80,17 +134,21 @@ app.post("/login", (req, res) => {
           page_name = "loggedin";
 
           //retrieving data
-          
-          db.collection("messages").find().toArray((err, results) => {
-            if (err) {
-              console.log(err);
-              res.render("auth/login", { page_name: page_name });
-            }
-            else {
-              let responses = results;
-              res.render("auth/showFormResponses", { page_name: page_name, responses:responses });
-            }
-          });
+
+          db.collection("messages")
+            .find()
+            .toArray((err, results) => {
+              if (err) {
+                console.log(err);
+                res.render("auth/login", { page_name: page_name });
+              } else {
+                let responses = results;
+                res.render("auth/showFormResponses", {
+                  page_name: page_name,
+                  responses: responses,
+                });
+              }
+            });
         } else {
           res.render("auth/login", { page_name: page_name });
           console.log("wrong password");
