988c236b853fd47bed9e61b78596f75e4624f5e9
manavendrasen <manavendra.199302058@muj.manipal.edu>
2020-11-19 21:50:41 +0530

    Added Credits route

--- routes/routes.js
+++ routes/routes.js
@@ -1,45 +1,48 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 //nav endpoints
 router.get("/", (req, res) => {
   const success = "false";
   const page_name = "home";
-  res.render("index", {page_name : page_name, success : success});
+  res.render("index", { page_name: page_name, success: success });
 });
 router.get("/tracks", (req, res) => {
   const page_name = "tracks";
-  res.render("tracks", {page_name : page_name});
+  res.render("tracks", { page_name: page_name });
 });
 router.get("/speakers", (req, res) => {
   const page_name = "speakers";
-  res.render("speakers", {page_name : page_name});
+  res.render("speakers", { page_name: page_name });
 });
 router.get("/schedule", (req, res) => {
   const page_name = "schedule";
-  res.render("schedule", {page_name : page_name});
+  res.render("schedule", { page_name: page_name });
 });
 router.get("/author-dashboard", (req, res) => {
   const page_name = "author-dashboard";
-  res.render("author-dashboard", {page_name : page_name});
+  res.render("author-dashboard", { page_name: page_name });
 });
 router.get("/committee", (req, res) => {
   const page_name = "committee";
-  res.render("committee", {page_name : page_name});
+  res.render("committee", { page_name: page_name });
 });
 router.get("/contact", (req, res) => {
   const page_name = "contact";
-  res.render("contact", {page_name : page_name});
+  res.render("contact", { page_name: page_name });
 });
 router.get("/register", (req, res) => {
   const page_name = "register";
-  res.render("register", {page_name : page_name});
+  res.render("register", { page_name: page_name });
 });
-
+router.get("/credits", (req, res) => {
+  const page_name = "credits";
+  res.render("credits", { page_name: page_name });
+});
 //auth
 router.get("/login", (req, res) => {
   const page_name = "login";
-  res.render("auth/login", {page_name : page_name});
+  res.render("auth/login", { page_name: page_name });
 });
 
 module.exports = router;
