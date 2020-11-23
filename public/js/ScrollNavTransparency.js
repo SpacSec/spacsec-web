b2de01d5e42312f2143c91173bceb6f6a705b30a
manavendrasen <manavendra.199302058@muj.manipal.edu>
2020-11-24 05:14:35 +0530

    added image to headers

--- public/js/ScrollNavTransparency.js
+++ public/js/ScrollNavTransparency.js
@@ -1,21 +1,33 @@
 const nav = document.getElementById("nav");
 const navToggler = document.querySelector(".navbar-toggler");
-
+// const bar = document.querySelectorAll(".navbar-link");
 window.onscroll = function () {
   "use strict";
-    if (
-      document.body.scrollTop >= 64 ||
-      document.documentElement.scrollTop >= 64
-    ) {
-      nav.classList.add("navbar-color");
-    } else {
-      nav.classList.remove("navbar-color");
-    }
+  if (
+    document.body.scrollTop >= 64 ||
+    document.documentElement.scrollTop >= 64
+  ) {
+    document.querySelectorAll(".navbar-link").forEach((el) => {
+      el.style.color = "#000";
+    });
+    document.querySelector(".fa-bars").style.color = "#000";
+    nav.classList.add("navbar-color");
+  } else {
+    document.querySelectorAll(".navbar-link").forEach((el) => {
+      el.style.color = "#fff";
+    });
+    document.querySelector(".fa-bars").style.color = "#fff";
+    nav.classList.remove("navbar-color");
+  }
 };
 
 // For responsive nav toggler
 const colorBackground = () => {
-  if (!(nav.classList.contains("navbar-color"))) {
+  document.querySelector(".fa-bars").style.color = "#000";
+  document.querySelectorAll(".navbar-link").forEach((el) => {
+    el.style.color = "#000";
+  });
+  if (!nav.classList.contains("navbar-color")) {
     nav.classList.toggle("navbar-color");
   }
 };
