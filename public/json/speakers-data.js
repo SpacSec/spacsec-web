76c12c0df8bb8eb297a3a0d833f8e570909f6d54
Aditya Khattar <khattaraditya2001@users.noreply.github.com>
2020-11-28 02:27:33 +0530

    Updated Speakers

--- public/json/speakers-data.js
+++ public/json/speakers-data.js
@@ -1,7 +1,27 @@
 const speakers = [
   {
+    name: "Prof. Walter Peeters",
+    image: "/images/Prof.WalterPeeters.jpg",
+    institute: "Technical University of Delft",
+    location: "Delft",
+    country: "Netherlands",
+    details: `
+    Has been with ESA since 1983 after initial activities in industry (construction, petrochemical). 
+    In ESA: project control and coordination on the Hermes project and the EUROMIR flights with Russia, 
+    among others. Former Head of the Coordination Office of the European Astronaut Centre in Cologne.
+    <br><br>
+    Lecturer in Space Marketing (Technical University of Delft) and former visiting professor 
+    in Non-profit Marketing (Catholic University of Louvain).
+    <br>
+    Founding Director of IISC (International Institute of Space Commerce)
+    <br>
+    Former Dean and President of ISU (till 2018)
+    `,
+    linkedin: "#",
+  },
+  {
     name: "Prof. Ajay Gupta",
-    image: "/images/speaker-1.jpg",
+    image: "/images/Prof.AjayGupta.jpg",
     institute: "Western Michigan University",
     location: "Kalamazoo",
     country: "MI",
@@ -11,7 +31,7 @@ const speakers = [
       Society being the Vice-Chair of the Technical Activities
       Committees in 2015-2016 and elected twice as the Chair of
       the Technical committee on Parallel Processing from 2011 to
-      2015. 
+      2015. <br><br>
       From 1998 to 2002, he was the Chairman of
       the Computer Science Department at Western Michigan
       University. Dr. Gupta received his Ph.D. in Computer Science
@@ -25,7 +45,7 @@ const speakers = [
   },
   {
     name: "Dr. Pljonkin A. Pavlovich",
-    image: "/images/speaker-2.png",
+    image: "/images/Dr.PljonkinA.Pavlovich.png",
     institute: "Ministry of Education and Science of the Russian Federation",
     location: "Southern Federal University",
     country: "",
@@ -36,7 +56,7 @@ const speakers = [
   },
   {
     name: "Anshuman Kalla",
-    image: "/images/speaker-3.png",
+    image: "/images/AnshumanKalla.png",
     institute: "Postdoctoral Visiting Researcher",
     location: "CWC",
     country: "University of Oulu, Finland",
