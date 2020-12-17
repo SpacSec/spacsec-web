0bb3d08ad31af51be440ae6d087d17154a10da76
Aditya Khattar <khattaraditya2001@users.noreply.github.com>
2020-12-17 14:53:44 +0530

    Reordered Speakers

--- public/json/speakers-data.js
+++ public/json/speakers-data.js
@@ -22,35 +22,51 @@ const speakers = [
     `,
     linkedin: "#",
   },
-  { //not yet available
-    name: "Dr. Anshuman Kalla",
-    image: "/images/AnshumanKalla.png",
-    institute: "Postdoctoral Visiting Researcher",
-    location: "CWC",
-    country: "University of Oulu, Finland",
+  {
+    name: "Dr. Hari B. Hablani",
+    image: "/images/Dr.HariB.Hablani.png",
+    institute: "Professor (Dept. of Aerospace Engineering)",
+    location: "Indian Institute of Technology, Bombay",
+    country: "India",
+    //details: `
+    //Prior to joining IITB, Dr. Hablani was a Visiting Faculty Professor at the 
+    //Department of Aerospace Engineering, IIT Kanpur, during July 2008 - July 2009, 
+    //where he taught three graduate courses concerning satellite orbits; spacecraft 
+    //guidance, navigation, and control; and integrated air navigation.  In addition, 
+    //he conducted research with B. Tech students concerning aircraft navigation aided 
+    //by ISRO’s GAGAN (GPS Aided Geostationary Augmented Navigation) and IRNSS 
+    //(Indian Regional Navigation Satellite System) programs; radar imaging surveillance 
+    //satellites; attitude determination, control, and GPS-based navigation of nanosatellites. 
+    //<br>
+    //Over the last five years, he has guided 15 MTech, Dual Degree and BTech students’ projects. 
+    //Some of these research areas are Spacecraft dynamics and control, Launch vehicles or Reusable 
+    //Launch Vehicles, Satellite-based air navigation, Missile Guidance and relative navigation, 
+    //Motion compensation of synthetic aperture radar on a cruise flight.
+    //`,
     details: `
-  
+    https://www.aero.iitb.ac.in/~hbhablani/
     `,
     linkedin: "#",
   },
-  {  
-    name: "Dr. Geeta Sikka",
-    image: "/images/Dr.GeetaSikka.png",
-    institute: "Associate Professor",
-    location: "NIT Jalandhar",
-    country: "India", 
+  { 
+    name: "Cem Avsar",
+    image: "/images/CemAvsar.png",
+    institute: "Managing Director at beSpace GmbH and Scientific Coordinator",
+    location: "TU, Berlin, Germany",
+    country: "",
     //details: `
-    //Dr Geeta Sikka is an associate professor at the National Institute of 
-    //Technology, Jalandhar. Her research interests lie in data mining, data 
-    //warehousing, databases, data science, big data and cloud computing. 
-    //<br><br>
-    //Dr Sikka completed her M.Tech in Computer Science from Punjab Agricultural 
-    //University in 2004. Later she earned a Ph.D. in Analysis & Design of 
-    //Analogy based Software Effort Estimation Approaches from NIT, Jalandhar. 
-    //Apart from this, she has also contributed to many chapters in various books.
+    //Cem Avsar is a managing director at beSpace GmbH and a research assistant 
+    //at TU Berlin. Being a Study Program Manager in the Master of Space Engineer 
+    //program, he has taught and supervised many students. Before this, he has 
+    //given lectures on satellite technology, aerospace technology, aerospace 
+    //electronics etc. He believes in hands-on learning and has introduced novel 
+    //ways to teach the students.<br><br>
+    //In 2010, he earned his diploma in Aerospace Engineering from TU 
+    //Berlin. He has also been a research associate in the iBOSS project and a 
+    //former CAD graphic designer at Ingencio, Berlin.
     //`,
     details: `
-    https://www.nitj.ac.in/index.php/nitj_cinfo/Faculty/30
+    https://www.raumfahrttechnik.tu-berlin.de/menue/ueber_uns/mitarbeiterinnen/dipl_ing_cem_avsar/
     `,
     linkedin: "#",
   },
@@ -75,46 +91,24 @@ const speakers = [
     `,
     linkedin: "#",
   },
-  { 
-    name: "Dr. B.B. Gupta",
-    image: "/images/Dr.B.B.Gupta.png",
-    institute: "Assistant Professor",
-    location: "NIT Kurukshetra",
-    country: "India",
+  {  
+    name: "Dr. Geeta Sikka",
+    image: "/images/Dr.GeetaSikka.png",
+    institute: "Associate Professor",
+    location: "NIT Jalandhar",
+    country: "India", 
     //details: `
-    //Dr B.B. Gupta is an assistant professor at the National Institute of 
-    //Technology, Kurukshetra. He has an experience of over ten years in teaching 
-    //and research and has published more than two hundred research papers. His 
-    //interests lie primarily in information security, cyber security, big data 
-    //analytics, botnet detection, network performance evaluation and much more.
+    //Dr Geeta Sikka is an associate professor at the National Institute of 
+    //Technology, Jalandhar. Her research interests lie in data mining, data 
+    //warehousing, databases, data science, big data and cloud computing. 
     //<br><br>
-    //He was awarded the Best Faculty Award at NIT Kurukshetra in 2019. Apart 
-    //from this, he was also a member of ACM, IEEE and SIGCOMM.
+    //Dr Sikka completed her M.Tech in Computer Science from Punjab Agricultural 
+    //University in 2004. Later she earned a Ph.D. in Analysis & Design of 
+    //Analogy based Software Effort Estimation Approaches from NIT, Jalandhar. 
+    //Apart from this, she has also contributed to many chapters in various books.
     //`,
     details: `
-    https://nitkkr.ac.in/comp_faculty_details.php?idd=96
-    `,
-    linkedin: "#",
-  },
-  { 
-    name: "Cem Avsar",
-    image: "/images/CemAvsar.png",
-    institute: "Managing Director at beSpace GmbH and Scientific Coordinator",
-    location: "TU, Berlin, Germany",
-    country: "",
-    //details: `
-    //Cem Avsar is a managing director at beSpace GmbH and a research assistant 
-    //at TU Berlin. Being a Study Program Manager in the Master of Space Engineer 
-    //program, he has taught and supervised many students. Before this, he has 
-    //given lectures on satellite technology, aerospace technology, aerospace 
-    //electronics etc. He believes in hands-on learning and has introduced novel 
-    //ways to teach the students.<br><br>
-    //In 2010, he earned his diploma in Aerospace Engineering from TU 
-    //Berlin. He has also been a research associate in the iBOSS project and a 
-    //former CAD graphic designer at Ingencio, Berlin.
-    //`,
-    details: `
-    https://www.raumfahrttechnik.tu-berlin.de/menue/ueber_uns/mitarbeiterinnen/dipl_ing_cem_avsar/
+    https://www.nitj.ac.in/index.php/nitj_cinfo/Faculty/30
     `,
     linkedin: "#",
   },
@@ -141,6 +135,27 @@ const speakers = [
     //`,
     details: `
     https://engineering.sfsu.edu/lecturer-profile-vidhyacharan-bhaskar
+    `,
+    linkedin: "#",
+  },
+  { 
+    name: "Dr. B.B. Gupta",
+    image: "/images/Dr.B.B.Gupta.png",
+    institute: "Assistant Professor",
+    location: "NIT Kurukshetra",
+    country: "India",
+    //details: `
+    //Dr B.B. Gupta is an assistant professor at the National Institute of 
+    //Technology, Kurukshetra. He has an experience of over ten years in teaching 
+    //and research and has published more than two hundred research papers. His 
+    //interests lie primarily in information security, cyber security, big data 
+    //analytics, botnet detection, network performance evaluation and much more.
+    //<br><br>
+    //He was awarded the Best Faculty Award at NIT Kurukshetra in 2019. Apart 
+    //from this, he was also a member of ACM, IEEE and SIGCOMM.
+    //`,
+    details: `
+    https://nitkkr.ac.in/comp_faculty_details.php?idd=96
     `,
     linkedin: "#",
   },
@@ -171,29 +186,14 @@ const speakers = [
     `,
     linkedin: "#",
   },
-  {
-    name: "Dr. Hari B. Hablani",
-    image: "/images/Dr.HariB.Hablani.png",
-    institute: "Professor (Dept. of Aerospace Engineering)",
-    location: "Indian Institute of Technology, Bombay",
-    country: "India",
-    //details: `
-    //Prior to joining IITB, Dr. Hablani was a Visiting Faculty Professor at the 
-    //Department of Aerospace Engineering, IIT Kanpur, during July 2008 - July 2009, 
-    //where he taught three graduate courses concerning satellite orbits; spacecraft 
-    //guidance, navigation, and control; and integrated air navigation.  In addition, 
-    //he conducted research with B. Tech students concerning aircraft navigation aided 
-    //by ISRO’s GAGAN (GPS Aided Geostationary Augmented Navigation) and IRNSS 
-    //(Indian Regional Navigation Satellite System) programs; radar imaging surveillance 
-    //satellites; attitude determination, control, and GPS-based navigation of nanosatellites. 
-    //<br>
-    //Over the last five years, he has guided 15 MTech, Dual Degree and BTech students’ projects. 
-    //Some of these research areas are Spacecraft dynamics and control, Launch vehicles or Reusable 
-    //Launch Vehicles, Satellite-based air navigation, Missile Guidance and relative navigation, 
-    //Motion compensation of synthetic aperture radar on a cruise flight.
-    //`,
+  { //not yet available
+    name: "Dr. Anshuman Kalla",
+    image: "/images/AnshumanKalla.png",
+    institute: "Postdoctoral Visiting Researcher",
+    location: "CWC",
+    country: "University of Oulu, Finland",
     details: `
-    https://www.aero.iitb.ac.in/~hbhablani/
+  
     `,
     linkedin: "#",
   },
