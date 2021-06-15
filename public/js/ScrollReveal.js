097ef1706b801ce414443d9320291de62a894eda
manavendrasen <manavendra.199302058@muj.manipal.edu>
2021-06-15 10:39:03 +0530

    feat: add scroll reveal animations
    - sections
    - cards
    - member names
    - timeline rows

---
+++ public/js/ScrollReveal.js
@@ -0,0 +1,5 @@
+ScrollReveal().reveal("section", { easing: "ease-in" });
+ScrollReveal().reveal(".card", { interval: 60, easing: "ease-in" });
+ScrollReveal().reveal(".timeline-row", { interval: 600, easing: "ease-in" });
+ScrollReveal().reveal(".member", { interval: 60, easing: "ease-in" });
+ScrollReveal().reveal(".roadmap-path");
