8aabd05ec3e57c7024d5fcacbd1c157e10775dbc
Manavendra Sen <manavendra4288@gmail.com>
2022-09-30 17:15:24 +0530

    feat: disabled form

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const routes = require("./routes/routes");
 const app = express();
 const mongoose = require("mongoose");
 const md5 = require("md5");
-require('dotenv').config()
+// require('dotenv').config()
 
 const speakers = require("./public/json/speakers-data");
 
@@ -15,107 +15,107 @@ if (port == null || port == "") {
 }
 
 //Database connection
-mongoose.connect(
-  process.env.DB_URL,
-  { useNewUrlParser: true, useUnifiedTopology: true }
-);
+// mongoose.connect(
+//   process.env.DB_URL,
+//   { useNewUrlParser: true, useUnifiedTopology: true }
+// );
 
-var db = mongoose.connection;
-var messageSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  message: {
-    type: String,
-    required: true,
-  },
-});
+// var db = mongoose.connection;
+// var messageSchema = new mongoose.Schema({
+//   name: {
+//     type: String,
+//     required: true,
+//   },
+//   email: {
+//     type: String,
+//     required: true,
+//   },
+//   message: {
+//     type: String,
+//     required: true,
+//   },
+// });
 
-messageSchema.set("timestamps", true);
-var userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+// messageSchema.set("timestamps", true);
+// var userSchema = new mongoose.Schema({
+//   username: {
+//     type: String,
+//     required: true,
+//   },
+//   password: {
+//     type: String,
+//     required: true,
+//   },
+// });
 
-const Message = mongoose.model("Message", messageSchema);
-app.post("/", (req, res) => {
-  const name = req.body.iname;
-  const email = req.body.iemail;
-  const message = req.body.imessage;
+// const Message = mongoose.model("Message", messageSchema);
+// app.post("/", (req, res) => {
+//   const name = req.body.iname;
+//   const email = req.body.iemail;
+//   const message = req.body.imessage;
 
-  console.log(name, email, message);
+//   console.log(name, email, message);
 
-  const mess = new Message({
-    name: name,
-    email: email,
-    message: message,
-  });
+//   const mess = new Message({
+//     name: name,
+//     email: email,
+//     message: message,
+//   });
 
-  mess.save();
-  const page_name = "home";
-  const success = "true";
-  const speakersData = speakers;
+//   // mess.save();
+//   const page_name = "home";
+//   const success = "true";
+//   const speakersData = speakers;
 
-  res.render("index", {
-    page_name: page_name,
-    success: success,
-    speakersData: speakersData,
-  });
-});
+//   res.render("index", {
+//     page_name: page_name,
+//     success: success,
+//     speakersData: speakersData,
+//   });
+// });
 
-const User = mongoose.model("User", userSchema);
-app.post("/login", (req, res) => {
-  const username = req.body.iusername;
-  const password = md5(req.body.ipassword);
-  let page_name = "login";
-  User.findOne({ username: username }, (err, foundUser) => {
-    if (err) {
-      console.log("nope");
-    } else {
-      if (foundUser) {
-        console.log("user found");
-        if (foundUser.password === password) {
-          console.log("correct password");
-          page_name = "loggedin";
+// const User = mongoose.model("User", userSchema);
+// app.post("/login", (req, res) => {
+//   const username = req.body.iusername;
+//   const password = md5(req.body.ipassword);
+//   let page_name = "login";
+//   User.findOne({ username: username }, (err, foundUser) => {
+//     if (err) {
+//       console.log("nope");
+//     } else {
+//       if (foundUser) {
+//         console.log("user found");
+//         if (foundUser.password === password) {
+//           console.log("correct password");
+//           page_name = "loggedin";
 
-          //retrieving data
+//           //retrieving data
 
-          db.collection("messages")
-            .find()
-            .toArray((err, results) => {
-              if (err) {
-                console.log(err);
-                res.render("auth/login", { page_name: page_name });
-              } else {
-                let responses = results;
-                res.render("auth/showFormResponses", {
-                  page_name: page_name,
-                  responses: responses,
-                });
-              }
-            });
-        } else {
-          res.render("auth/login", { page_name: page_name });
-          console.log("wrong password");
-        }
-      } else {
-        console.log("user not found");
-        res.render("auth/login", { page_name: page_name });
-      }
-    }
-  });
-});
+//           db.collection("messages")
+//             .find()
+//             .toArray((err, results) => {
+//               if (err) {
+//                 console.log(err);
+//                 res.render("auth/login", { page_name: page_name });
+//               } else {
+//                 let responses = results;
+//                 res.render("auth/showFormResponses", {
+//                   page_name: page_name,
+//                   responses: responses,
+//                 });
+//               }
+//             });
+//         } else {
+//           res.render("auth/login", { page_name: page_name });
+//           console.log("wrong password");
+//         }
+//       } else {
+//         console.log("user not found");
+//         res.render("auth/login", { page_name: page_name });
+//       }
+//     }
+//   });
+// });
 
 //show responses
 
